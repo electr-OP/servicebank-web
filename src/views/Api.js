0ad15ff8a4dde82b69ea2662c0e3b86fd28ae920
Electr-Op <daelectropel@gmail.com>
2022-12-14 16:12:24 +0100

    latest

--- src/views/Api.js
+++ src/views/Api.js
@@ -19,12 +19,14 @@ let token = await localStorage.getItem('token')
     }
   token =  JSON.parse(token)?.access
   token ? config.headers.Authorization = `Bearer ${token}` : null;
-  config.headers['Access-Key'] = 'ChFwjUfM.gl9elkgX3RCiXRmVrOqogltEarg8vbEq';
+  // config.headers['Access-Key'] = 'ChFwjUfM.gl9elkgX3RCiXRmVrOqogltEarg8vbEq';
+  config.headers['Access-Key'] = 'eFk4JQ1D.LIkXkb3NGakZG0obYQhYOS2anplmxAma'
   return config;
 });
 // const baseUrl = 'https://09c2-102-89-23-110.eu.ngrok.io/api/'
 
-const baseUrl = '//localhost:8000/';
+// const baseUrl = '//localhost:8000/';
+const baseUrl = 'http://50.19.18.134/';
 // axios_instance.interceptors.request.use(config => {
 //   this.$progress.start() 
 //   return configDoctor
